ad9de3aa62316c10f6015ca2a2b2a82af45d8d15
Khazov Anton <ulowpie@gmail.com>
2024-06-19 17:13:41 +0500

    add color

--- apkquick.sh
+++ apkquick.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 app=''
 v_flag=0
 die() { exit 1; }
@@ -13,9 +18,19 @@ print_logo() {
 print_usage() { echo "Usage: appquick [-v] -a <appname.apk>";}
 print_verbose() {
     if [ $v_flag -eq 1 ] ; then
-        echo -e "$1"
+        print_blue $1;
     fi
 }
+print_green() {
+    echo -e "$GREEN$1$NC"
+}
+print_red() {
+    echo -e "$RED$1$NC"
+}
+print_blue() {
+    echo -e "$BLUE$1$NC"
+}
+
 
 if [[ $# -eq 0 ]] ; then
     print_logo;
@@ -38,16 +53,19 @@ if [ -z $app ] ; then
 fi
 
 # Static info
+print_verbose "Parse aapt dump badging and get pkg info"
 pkg=$(aapt dump badging $app|awk -F" " '/package/ {print $2}'|awk -F"'" '/name=/ {print $2}')
 act=$(aapt dump badging $app|awk -F" " '/launchable-activity/ {print $2}'|awk -F"'" '/name=/ {print $2}')
 ver=$(aapt dump badging $app|awk -F" " '/package/ {print $4}'|awk -F"'" '/versionName=/ {print $2}')
 
+print_green "Simple info"
 echo "Package Name: $pkg"
 echo "MainActivity: $act"
 echo "Version: $ver"
 
+print_verbose "\nParse aapt permissions"
 perm=$(aapt d permissions $app | grep "permission:")
-echo "Uses Permissions:"
+print_green "Uses Permissions:"
 IFS=$'\n'
 for LINE in $perm
 do
@@ -55,7 +73,7 @@ do
         echo " - `echo ${LINE} | cut -d \' -f 2`";
     fi
 done
-echo "Defines Permissions:"
+print_green "Defines Permissions:"
 for LINE in $perm
 do
     if [[ $LINE != *"uses-permission:"* ]]; then
@@ -63,33 +81,42 @@ do
     fi
 done
 
+echo ""
+print_verbose "Find device connection"
 dp="device product:"
 dev=$(adb devices -l 2>/dev/null | grep "$dp")
 if [ $? -ne 0 ] ; then
     print_verbose "No device connections";
     die;
 fi
+print_verbose "Device is found"
 
 pml=$(adb shell pm list package | grep "$pkg")
 if [[ "${dev}" == *"${dp}"* ]] && [[ "${pml}" == *"${pkg}"* ]] ; then
     # If this pkg is installed
-    echo -e "\nDevice Info"
+    echo ""
+    print_verbose "Get info of device with adb shell getprop"
+    print_green "Device Info"
     echo "IP: `echo $dev | cut -d ':' -f 1`"
     echo "Device: `echo $dev | awk -F "$dp" '{print $2}'`"
     echo "Version Android: `adb shell getprop ro.build.version.release`"
     echo "SDK Version: `adb shell getprop ro.build.version.sdk`"
 
-    echo -e "\nPackage Info"
+    echo ""
+    print_verbose "Get location info installed app"
+    print_green "Package Info"
     echo "Path APK: `adb shell pm path $pkg | cut -d ':' -f 2`"
     echo "Path Data: /data/data/$pkg"
     echo "Link Data: /data/user/0/$pkg"
 
+    echo ""
+    print_verbose "Get location info with find /data command"
     adb root >/dev/null
     if [ $? -eq 0 ] ; then
-        echo -e "\nOther Directories\n$(adb shell find /data -name "$pkg")"
+        print_green "Other Directories"
+        echo "$(adb shell find /data -name "$pkg")"
     else
         print_verbose "ADB root restart failed"
     fi
 fi
-
 
