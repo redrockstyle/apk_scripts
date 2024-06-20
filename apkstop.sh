ae98ce4a622ef2185b99120be3d1ffee7dde0ef3
Khazov Anton <ulowpie@gmail.com>
2024-06-20 16:17:18 +0500

    implemented in apprun

--- apkstop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $# -eq 0 ]] ; then
-    echo "Usage: appstop <appname.apk>"
-    exit 1
-fi
-
-pkg=$(aapt dump badging $1|awk -F" " '/package/ {print $2}'|awk -F"'" '/name=/ {print $2}')
-adb shell am force-stop $pkg
